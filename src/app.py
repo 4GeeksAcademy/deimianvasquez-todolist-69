b4b7e6e7fb19590cf2c16f0157d10594e077763b
Deimian Vásquez <deimianvasquez@gmail.com>
2026-03-31 00:32:18 +0000

    add todos

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Todo
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,25 +28,52 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/users/<string:user_name>', methods=['POST'])
+def create_user(user_name):
+    # select * from user where name="deimian"
+    existing_user = User.query.filter_by(name=user_name.lower()).first()
+    if existing_user is not None:
+        return jsonify("user already exists"), 400
 
-    return jsonify(response_body), 200
+    user = User(name=user_name)  # name --> deimian
+    db.session.add(user)
+    try:
+        db.session.commit()
+        return jsonify(user.serialize()), 201
+    except Exception as err:
+        db.sesion.rollback()
+        return jsonify({"error": f"Error: {err.args}"})
+
+
+@app.route("/users", methods=["GET"])
+def get_all_users():
+    users = User.query.all()
+    print(users)
+    # result_body = list(map(lambda item: item.serialize(), users))
+    return jsonify({"users": [user.serialize() for user in users]}), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+    """
+        1.- terminar los endpoints
+        2.- lanzar a producción (render.com)
+        3.- probar al azar con uno hecho de los estudiante
+    """
--- src/models.py
+++ src/models.py
@@ -1,19 +1,57 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, DateTime, func, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+class User(db.Model):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now,
+        nullable=False
+    )
+
+    todos: Mapped[list['Todo']] = relationship(
+        "Todo",
+        back_populates="user",
+        uselist=True  # viene por default
+    )
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "created_at": self.created_at.isoformat()
         }
+
+
+class Todo(db.Model):
+    __table_name__ = "todo"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    label: Mapped[str] = mapped_column(String(255), nullable=False)
+    id_done: Mapped[bool] = mapped_column(
+        Boolean(), nullable=False, default=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False
+    )
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    user: Mapped['User'] = relationship(
+        "User",
+        back_populates="todos"
+    )
