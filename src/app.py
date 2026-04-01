cdec65c9247f88528c31aa70fe917e124f42b5c3
Deimian Vásquez <deimianvasquez@gmail.com>
2026-04-01 23:15:12 +0000

    ended endpoint

--- src/app.py
+++ src/app.py
@@ -46,11 +46,12 @@ def sitemap():
 def create_user(user_name):
     # select * from user where name="deimian"
     existing_user = User.query.filter_by(name=user_name.lower()).first()
+
     if existing_user is not None:
         return jsonify("user already exists"), 400
 
     user = User(name=user_name)  # name --> deimian
-    db.session.add(user)
+    db.session.add(user)  # crea a la transacción de db
     try:
         db.session.commit()
         return jsonify(user.serialize()), 201
@@ -61,19 +62,111 @@ def create_user(user_name):
 
 @app.route("/users", methods=["GET"])
 def get_all_users():
-    users = User.query.all()
-    print(users)
-    # result_body = list(map(lambda item: item.serialize(), users))
-    return jsonify({"users": [user.serialize() for user in users]}), 200
+    try:
+        users = User.query.all()
+
+        # result_body = list(map(lambda item: item.serialize(), users))
+        return jsonify({"users": [user.serialize() for user in users]}), 200
+
+    except Exception as err:
+        return jsonify({"message": f"Error: {err.args}"}), 500
 
 
-# this only runs if `$ python src/app.py` is executed
+@app.route("/users/<string:user_name>", methods=["DELETE"])
+def delete_user(user_name):
+    user = User.query.filter_by(name=user_name).first()
+
+    if user is None:
+        # raise APIException("User not fount", status_code=404)
+        return jsonify({"message": "User not found"}), 404
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({}), 204
+
+
+@app.route("/users/<string:user_name>", methods=["GET"])
+def read_user(user_name):
+
+    user = User.query.filter_by(name=user_name.lower()).first()
+    if user is None:
+        return jsonify({"message": "User not found"}), 404
+
+    return jsonify(user.serialize_with_items())
+
+
+@app.route("/todos/<string:user_name>", methods=["POST"])
+def create_user_todo(user_name):
+    body = request.get_json(silent=True) or {}
+    label = body.get("label")
+    is_done = body.get("is_done", False)
+
+    if not isinstance(label, str) or label.strip() == '':
+        raise APIException(
+            "label required an must be a string", status_code=400)
+
+    if not isinstance(is_done, bool):
+        return jsonify({"message": "is_done must be a bolean"}), 400
+
+    user = User.query.filter_by(name=user_name.lower()).first()
+    if user is None:
+        return jsonify({"message": "User not found"}), 404
+
+    todo = Todo(label=label.strip(), is_done=is_done, user_id=user.id)
+    db.session.add(todo)
+    try:
+        db.session.commit()
+        return jsonify(todo.serialize()), 201
+    except Exception as err:
+        return jsonify({"message": f"Error: {err.args}"}), 500
+
+
+@app.route("/todos/<int:todo_id>", methods=["PUT"])
+def update_user_todo(todo_id):
+    body = request.get_json(silent=True) or {}
+
+    todo = Todo.query.get(todo_id)
+    if todo is None:
+        return jsonify({"message": "Todo not found"}), 404
+
+    if 'label' in body and body['label'] is not None:
+        if not isinstance(body["label"], str) or body["label"].strip() == "":
+            raise APIException(
+                "label required an must be a string", status_code=400)
+        todo.label = body["label"].strip()
+
+    if 'is_done' in body and body['is_done'] is not None:
+        if not isinstance(body["is_done"], bool):
+            return jsonify({"message": "is_done must be a bolean"}), 400
+        todo.is_done = body["is_done"]
+
+    try:
+        db.session.commit()
+        return jsonify(todo.serialize()), 200
+    except Exception as err:
+        return jsonify({"message": f"Error: {err.args}"}), 500
+
+
+@app.route("/todos/<int:todo_id>", methods=["DELETE"])
+def delete_task(todo_id):
+    todo = Todo.query.get(todo_id)
+
+    if todo is None:
+        return jsonify({"message": "Todo not found"}), 404
+
+    db.session.delete(todo)
+    db.session.commit()
+
+    return jsonify({}), 204
+
+
+    # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
 
     """
-        1.- terminar los endpoints
         2.- lanzar a producción (render.com)
         3.- probar al azar con uno hecho de los estudiante
     """
--- src/models.py
+++ src/models.py
@@ -27,10 +27,17 @@ class User(db.Model):
     )
 
     def serialize(self):
+        # code de python
         return {
             "id": self.id,
             "name": self.name,
             "created_at": self.created_at.isoformat()
+        }
+
+    def serialize_with_items(self):
+        return {
+            "name": self.name,
+            "todos": [todo.serialize() for todo in self.todos]
         }
 
 
@@ -39,7 +46,7 @@ class Todo(db.Model):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     label: Mapped[str] = mapped_column(String(255), nullable=False)
-    id_done: Mapped[bool] = mapped_column(
+    is_done: Mapped[bool] = mapped_column(
         Boolean(), nullable=False, default=False)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), nullable=False)
@@ -55,3 +62,11 @@ class Todo(db.Model):
         "User",
         back_populates="todos"
     )
+
+    def serialize(self):
+        return {
+            "label": self.label,
+            "is_done": self.is_done,
+            "id": self.id,
+            "created_at": self.created_at.isoformat()
+        }
